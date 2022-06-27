5fcaff98d180b70ccc2fb559f4df80f6234a081f
KirillPushkin <benberov09@mail.ru>
2022-06-27 16:42:16 +0300

    Commit_1

---
+++ src/test/java/ru/gb/wtg/WtgApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.wtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
