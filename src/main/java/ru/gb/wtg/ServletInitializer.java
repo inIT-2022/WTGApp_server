b998bb3911846cc9e97b01f278a9731af7af2f18
KirillPushkin <benberov09@mail.ru>
2022-06-28 12:12:50 +0300

    Commit_1

--- src/main/java/ru/gb/wtg/ServletInitializer.java
+++ src/main/java/ru/gb/wtg/ServletInitializer.java
@@ -8,8 +8,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 
-
-
         return application.sources(WtgApplication.class);
     }
 
