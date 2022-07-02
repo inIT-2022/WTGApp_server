aa8457fb925ea19583784f61039ca9043c19b55d
Leucif <71128490+Leucif@users.noreply.github.com>
2022-07-02 11:47:43 +0300

    parser-base-methods

---
+++ src/test/java/ru/gb/parser/DataBox.java
@@ -0,0 +1,49 @@
+package base;
+
+import java.util.ArrayList;
+
+public class DataBox {
+    ArrayList images;
+    ArrayList text;
+    
+    
+    
+    
+	public ArrayList getImages() {
+		return images;
+	}
+
+
+
+
+	public void setImages(ArrayList images) {
+		this.images = images;
+	}
+
+
+
+
+	public ArrayList getText() {
+		return text;
+	}
+
+
+
+
+	public void setText(ArrayList text) {
+		this.text = text;
+	}
+
+
+
+
+	public DataBox(ArrayList images, ArrayList text) {
+		super();
+		this.images = images;
+		this.text = text;
+	}
+    
+	
+    
+    
+}
---
+++ src/test/java/ru/gb/parser/Main.java
@@ -0,0 +1,20 @@
+package base;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		HTMLParser parser = new HTMLParser();
+		String[] pages = parser.getPages("https://tourism.krd.ru", "gallery");
+		//parser.getContent("https://tourism.krd.ru", pages, "hotel");
+		DataBox result = parser.getContent("https://www.culture.ru/institutes/10078/gosudarstvennaya-tretyakovskaya-galereya", "https://www.culture.ru" , "KRQ9s");
+		System.out.print(result.images.get(0));
+	}
+
+}
+
