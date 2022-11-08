8a5572621bd408c9a636a4c8d5a883180d15973b
HeleneElena <heleneebert1@gmail.com>
2022-11-08 10:44:56 +0100

    fix: layout

--- frontend/src/components/Main/Location/Location2/Location2.jsx
+++ frontend/src/components/Main/Location/Location2/Location2.jsx
@@ -1,5 +1,4 @@
 import style from './Location2.module.css';
-import { ReactComponent as SearchSvg } from '../img/search.svg';
 import { TextLocation } from './TextLocation/TextLocation';
 
 export const Location2 = () => {
