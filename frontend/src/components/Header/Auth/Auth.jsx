7e353f0e267f0d2d7073dc8c6a181dd29427fa79
DashkevichAS <antonsnov0604@gmail.com>
2022-11-23 16:50:33 +0300

    add Form

--- frontend/src/components/Header/Auth/Auth.jsx
+++ frontend/src/components/Header/Auth/Auth.jsx
@@ -5,25 +5,34 @@ import { RegistrationForm } from '../../RegistrationForm/RegistrationForm';
 import { useState } from 'react';
 
 export const Auth = () => {
-  const [showModalForm,  setShowModalForm] = useState(false);
+  const [showModalForm, setShowModalForm] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      {
-        isOpen && (
-          <div className={style.auth__text} >
-              <p className={style.auth__user_name}>Добро пожаловать, Павел!</p>
-          </div>
-        )
-      }
-      
-      <button className={style.auth__button} onClick={() => setShowModalForm(!showModalForm)}>
-        <AuthIcon className={style.auth__icon} onClick={() => setIsOpen(!isOpen)} />
+      {isOpen && (
+        <div className={style.auth__text}>
+          <p className={style.auth__user_name}>Добро пожаловать, Павел!</p>
+        </div>
+      )}
+
+      <button
+        className={style.auth__button}
+        onClick={() => setShowModalForm(!showModalForm)}
+      >
+        <AuthIcon
+          className={style.auth__icon}
+          onClick={() => setIsOpen(!isOpen)}
+        />
       </button>
-      
-      <CSSTransition classNames='alert' in={showModalForm} timeout={800} unmountOnExit>
-          <RegistrationForm />
+
+      <CSSTransition
+        classNames='alert'
+        in={showModalForm}
+        timeout={10000}
+        unmountOnExit
+      >
+        <RegistrationForm />
       </CSSTransition>
     </div>
   );
