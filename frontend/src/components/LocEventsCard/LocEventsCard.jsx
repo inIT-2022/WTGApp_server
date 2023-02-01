fb107b6d31087c0a4ca78e23976ac43778cced8e
DashkevichAS <antonsnov0604@gmail.com>
2023-02-01 20:48:53 +0300

    fix LocEventsCard  linkImage

--- frontend/src/components/LocEventsCard/LocEventsCard.jsx
+++ frontend/src/components/LocEventsCard/LocEventsCard.jsx
@@ -2,9 +2,7 @@ import { Link } from 'react-router-dom';
 import style from './LocEventsCard.module.css';
 import noPhoto from '../../img/nophoto.jpg';
 export const LocEventsCard = ({ event }) => {
-  const imgLink = event.location?.linkImage
-    ? event.location.linkImage?.split('|')[0]
-    : '';
+  const imgLink = event?.linkImage ? event.linkImage?.split('|')[0] : '';
   return (
     <Link to={`/events/${event.id}`} className={style.cardWrapper}>
       <img
